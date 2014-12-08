f43d7e6d02b8ee0a277cff57a3f08f3a341b179f
Thomson <lilotom@gmail.com>
2014-12-08 18:02:10 +0800

    Fix incorrectness of search result and rename mf.pl to gf.cmd, part 2

--- gf.cmd
+++ gf.cmd
@@ -1,3 +1,11 @@
+@rem = '--*-Perl-*--
+@echo off
+perl -x -S %0 %*
+goto endofperl
+
+
+@rem -- BEGIN PERL -- ';
+#line 8
 #!/usr/bin/perl
 # Search keywords in multiple lines in given files, or file names in given pattern
 # under current directory
@@ -7,7 +15,7 @@ use File::Find;
 
 my $pat1;
 my $pat2;
-my $minLines = 1;
+my $minLines = 2;
 my $maxLines = 10;
 my $inverse_search;
 my @Filter_ARGV;
@@ -16,14 +24,16 @@ my $FileNamePattern;
 for (@ARGV) {
     if (/^-l/) {
         $minLines = substr $_, 2;
+        $minLines += 1;
     }
     elsif (/^-L/) {
         $maxLines = substr $_, 2;
+        $maxLines += 1;
     }
     elsif (/^\-r/) {
         $inverse_search = true;
     }
-    elsif (/^-R/) {
+    elsif (/^\-R/) {
         $FileNamePattern = substr $_, 2;
     }
     elsif (!defined($pat1)) {
@@ -51,8 +61,8 @@ sub GetFileNames {
 }
 
 find ({wanted => \&GetFileNames, no_chdir => 1}, '.');
-if ($FileNamePattern && @Filter_ARGV == 0) {
-    exit 0;
+if (not $FileNamePattern && @Filter_ARGV == 0) {
+    #exit 0;
 }
 
 @ARGV = @Filter_ARGV;
@@ -63,29 +73,32 @@ my $start_matching = 0;
 my $hasAnyOutput = 0;
 while (<>) {
    chomp;
+   if (/$pat1/i) {
+       @cache_range = ();
+       $start_matching = 1;
+       $startline = $.;
+       push @cache_range, $_;
+       next;
+   }
    if ($start_matching) {
        push @cache_range, $_;
        if (@cache_range < $minLines) {
            next;
        }
        elsif (@cache_range > $maxLines) {
-           $start_matching = false;
+           $start_matching = 0;
            @cache_range = ();
        }
        elsif (/$pat2/i) {
-           for (@cache_range) {
+            for (@cache_range) {
                print $ARGV, ':', $startline++, ': ', $_, "\n";
                $hasAnyOutput = 1;
            }
+           print "----------------------------------------\n";
 
-           $start_matching = false;
+           $start_matching = 0;
            @cache_range = ();
        }
-   }
-   elsif (/$pat1/i) {
-       $start_matching = 1;
-       $startline = $.;
-       push @cache_range, $_;
    }
 } continue {
     if (eof) {
@@ -93,8 +106,13 @@ while (<>) {
         $start_matching = 0;
         @cache_range = ();
         if ($hasAnyOutput ) {
-            print "--------------------------------------------------------------------------------\n";
+            print "================================================================================\n";
             $hasAnyOutput = 0;
         }
     }
 }
+
+
+__END__
+:endofperl
+
