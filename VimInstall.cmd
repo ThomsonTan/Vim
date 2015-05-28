06c4321760e0f065089ccb850c08dd9d3c41bc92
Thomson <lilotom@gmail.com>
2015-05-28 18:26:35 +0800

    Update installation script to reflect new folder structure

--- VimInstall.cmd
+++ VimInstall.cmd
@@ -2,19 +2,4 @@
 
 cmd.exe /c copy /y /v _vimrc %homedrive%%homepath%\_vimrc
 
-if not exist %homedrive%%homepath%\vimfiles\colors (
-mkdir %homedrive%%homepath%\vimfiles\colors
-)
-copy colors\tan.vim %homedrive%%homepath%\vimfiles\colors\tan.vim
-
-if not exist %homedrive%%homepath%\vimfiles\plugin (
-mkdir %homedrive%%homepath%\vimfiles\plugin
-)
-copy plugin\srcexpl.vim %homedrive%%homepath%\vimfiles\plugin\
-copy plugin\taglist.vim %homedrive%%homepath%\vimfiles\plugin\
-
-if not exist %homedrive%%homepath%\vimfiles\after\syntax (
-mkdir %homedrive%%homepath%\vimfiles\after\syntax
-)
-
-copy after\syntax\c.vim %homedrive%%homepath%\vimfiles\after\syntax\
+robocopy /S vimfiles\ %homedri%%homepath%\
