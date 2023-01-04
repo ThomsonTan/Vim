01cf7456190b64aac79deb40087658a8c4e99625
Tom Tan <Tom.Tan@microsoft.com>
2023-01-04 11:28:01 -0800

    Don't install ctags and no longer need admin

--- Install.cmd
+++ Install.cmd
@@ -1,39 +1,39 @@
-@REM workaround to check running in admin mode or not
+@REM Don't rely on admin mode
 @echo off
-reg query HKLM\Software\TestElevated-f57142fd-2586-45b8-bdb6-49e98d93789b >NUL 2>NUL && goto :TestKeyExists
-reg add HKLM\Software\TestElevated-f57142fd-2586-45b8-bdb6-49e98d93789b /f >NUL 2>NUL || goto :ErrorNotAdmin
-reg delete HKLM\Software\TestElevated-f57142fd-2586-45b8-bdb6-49e98d93789b /f >NUL 2>NUL || goto :FailedToDelete
+@REM reg query HKLM\Software\TestElevated-f57142fd-2586-45b8-bdb6-49e98d93789b >NUL 2>NUL && goto :TestKeyExists
+@REM reg add HKLM\Software\TestElevated-f57142fd-2586-45b8-bdb6-49e98d93789b /f >NUL 2>NUL || goto :ErrorNotAdmin
+@REM reg delete HKLM\Software\TestElevated-f57142fd-2586-45b8-bdb6-49e98d93789b /f >NUL 2>NUL || goto :FailedToDelete
 @echo Install customized Vim stuff to system
 
 cmd.exe /c copy /y /v %~dp0_vimrc %homedrive%%homepath%\_vimrc
 
 robocopy /S %~dp0vimfiles\ %homedrive%%homepath%\vimfiles\
 
-if "%PROCESSOR_ARCHITECTURE%" equ "AMD64" (
-    cmd.exe /c copy /y /v %~dp0VimBin\ctags.exe %systemroot%\syswow64\
-) else (
-    cmd.exe /c copy /y /v %~dp0VimBin\ctags.exe %systemroot%\system32\
-)
+@REM if "%PROCESSOR_ARCHITECTURE%" equ "AMD64" (
+@REM    cmd.exe /c copy /y /v %~dp0VimBin\ctags.exe %systemroot%\syswow64\
+@REM ) else (
+@REM    cmd.exe /c copy /y /v %~dp0VimBin\ctags.exe %systemroot%\system32\
+@REM )
 
 goto :Success
 
-:FailedToDelete
-@echo ========================================
-@echo How could this happen? Ping lilotom@gmail.com
-@echo ========================================
-exit /b 3
-
-:TestKeyExists
-@echo ========================================
-@echo Why do you have this key?
-@echo ========================================
-exit /b 1
-
-:ErrorNotAdmin
-@echo ========================================
-@echo Please install in administrator mode!!!
-@echo ========================================
-exit /b 2
+@REM :FailedToDelete
+@REM @echo ========================================
+@REM @echo How could this happen?
+@REM @echo ========================================
+@REM exit /b 3
+@REM 
+@REM :TestKeyExists
+@REM @echo ========================================
+@REM @echo Why do you have this key?
+@REM @echo ========================================
+@REM exit /b 1
+@REM 
+@REM :ErrorNotAdmin
+@REM @echo ========================================
+@REM @echo Please install in administrator mode!!!
+@REM @echo ========================================
+@REM exit /b 2
 
 :Success
 @echo ========================================
